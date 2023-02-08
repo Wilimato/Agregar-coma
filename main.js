9aea44529878040414f2c52afdc7ce72dc577ea3
Wilmar Acuña <wilimato09@gmail.com>
2023-02-07 21:40:35 -0300

    Modicacion de comandos y eliminacion de formulario

--- main.js
+++ main.js
@@ -5,17 +5,33 @@ let mensaje = document.getElementById("offMensaje");
 let input = document.querySelector("input");
 
 
-enviar.addEventListener("click", function(){
+
+
+function getOutput (){
     let input = document.querySelector("input").value;
     let resultado = input.replaceAll(" ", ", ");
     document.getElementById("resultado").value = resultado;
-})
+}
 
-button.addEventListener("click", function(){
+function copy(){
     document.querySelector(".resultado").focus();
     document.execCommand("selectAll");
     document.execCommand("copy");
     mensaje.classList.add("activo");
     setTimeout(()=>mensaje.classList.remove("activo"), 4000);
 
-})
+}
+
+enviar.addEventListener("click", getOutput);
+
+button.addEventListener("click", copy);
+
+document.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        getOutput();
+    } else if (event.keyCode === 67) {
+        copy();
+    }
+} );
+
+
