e02b2a056796a8f0f6b66d7e5a4f10f32c5fd167
Wilmar Acuña <wilimato09@gmail.com>
2023-02-25 13:55:12 -0300

    Corregir Enter

--- main.js
+++ main.js
@@ -4,19 +4,6 @@ let output = document.querySelector(".resultado");
 let mensaje = document.getElementById("offMensaje");
 let input = document.querySelector("input");
 
-
-function noEnter($id){
-    $textarea = document.getElementById($id);
-    niveles = -1;
-     
-    if(event.which != null) { // Navegadores compatibles con Mozilla
-        if(event.which == 13){
-            if(navigator.appName == "Opera") niveles = -2;
-            $textarea.value = $textarea.value.slice(0, niveles);
-        }
- }
-}
-
 function getOutput (){
     let input = document.querySelector(".input").value;
     let resultado = input.replaceAll(" ", ", ");
@@ -38,11 +25,18 @@ button.addEventListener("click", copy);
 
 document.addEventListener("keyup", function(event) {
     if (event.keyCode == 13) {
-        noEnter("input");
         getOutput();
     } else if (event.keyCode === 67) {
         copy();
     }
 } );
+
+function pulsar(e) {
+    if (e.which === 13) {
+      e.preventDefault();
+      console.log('prevented');
+      return false;
+    }
+  }
 
 
