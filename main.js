dc900ec5dba245e134eb91ee5c05f313eaf6d973
Wilmar Acuña <wilimato09@gmail.com>
2023-05-29 19:00:29 -0400

    agregado remplazar salto de linea

--- main.js
+++ main.js
@@ -7,41 +7,42 @@ let input = document.querySelector("input");
 //Desactivar tecla "Enter"
 function pulsar(e) {
     if (e.which === 13) {
-      e.preventDefault();
-      console.log('prevented');
-      return false;
+        e.preventDefault();
+        console.log('prevented');
+        return false;
     }
-  }
+}
 
 
 //Remplazar espacio por coma 
-function getOutput (){
+function getOutput() {
     let input = document.querySelector(".input").value;
-    let resultado = input.replaceAll(" ", ", ");
+    let resultado = input.replaceAll(/[ \n]+/g, ", ");
     document.getElementById("resultado").value = resultado;
 }
 
 //Copiar resultado
-function copy(){
+function copy() {
     document.querySelector(".resultado").focus();
     document.execCommand("selectAll");
     document.execCommand("copy");
     mensaje.classList.add("activo");
-    setTimeout(()=>mensaje.classList.remove("activo"), 4000);
+    setTimeout(() => mensaje.classList.remove("activo"), 4000);
 
 }
+
 
 enviar.addEventListener("click", getOutput);
 
 button.addEventListener("click", copy);
 
-document.addEventListener("keyup", function(event) {
+document.addEventListener("keyup", function (event) {
     if (event.keyCode == 13) {
         getOutput();
     } else if (event.keyCode === 67) {
         copy();
     }
-} );
+});
 
 
 
